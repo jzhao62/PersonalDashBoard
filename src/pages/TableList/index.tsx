21afa5c9ca53c5787f6258d200c450430f83c19c
Jing Yi Zhao <jingyi@pm61data.com>
2021-05-28 21:53:49 -0700

    init

--- src/pages/TableList/index.tsx
+++ src/pages/TableList/index.tsx
@@ -11,6 +11,7 @@ import ProDescriptions from '@ant-design/pro-descriptions';
 import type { FormValueType } from './components/UpdateForm';
 import UpdateForm from './components/UpdateForm';
 import { rule, addRule, updateRule, removeRule } from '@/services/ant-design-pro/rule';
+import styles from './index.less';
 
 /**
  * 添加节点
@@ -212,6 +213,7 @@ const TableList: React.FC = () => {
   return (
     <PageContainer>
       <ProTable<API.RuleListItem, API.PageParams>
+        className={styles.proTableContainer}
         headerTitle={intl.formatMessage({
           id: 'pages.searchTable.title',
           defaultMessage: '查询表格',
